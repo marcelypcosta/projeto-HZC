1f8232e9b3886a56897bc835058a10d920eab41f
Marcely Pereira Costa <101207575+marcelypcosta@users.noreply.github.com>
2024-04-29 15:12:35 -0300

    Delete Projeto HZC directory

--- Projeto HZC/index.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho_menu') // acessar o seletor com a classe .cabecalho_menu
-const menu = document.querySelector('.menu-lateral') // acessar o seletor com a classe .menu-lateral
-
-botaoMenu.addEventListener('click', () =>{
-    menu.classList.toggle('menu-lateral--ativo')
-})
